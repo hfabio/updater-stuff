a3aa639cbe369423e740f38fa5514df5a4210c4d
Helton Fábio <heltonfabio@outlook.com>
2020-07-13 18:49:24 -0300

    fixing skype launcher name

--- update-skype.sh
+++ update-skype.sh
@@ -2,6 +2,6 @@
 notify-send "Atualizando o skype, $USER"
 wget https://repo.skype.com/latest/skypeforlinux-64.deb -O /tmp/skypeforlinux.deb
 sudo dpkg -i /tmp/skypeforlinux.deb
-sudo killall skype
-skype
+sudo killall skypeforlinux
+skypeforlinux
 notify-send "Skype atualizado"
