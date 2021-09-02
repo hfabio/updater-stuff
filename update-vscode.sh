b0d8827af2b39c124b190ab07689c16f062f261e
Helton Fábio <heltonfabio@outlook.com>
2021-09-02 20:46:53 -0300

    Update update-vscode.sh

--- update-vscode.sh
+++ update-vscode.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 notify-send "Atualizando o vscode, $USER"
-wget 'https://vscode-update.azurewebsites.net/latest/linux-deb-x64/stable' -O /tmp/code_latest_amd64.deb
+wget 'https://code.visualstudio.com/sha/download?build=stable&os=linux-deb-x64' -O /tmp/code_latest_amd64.deb
 sudo dpkg -i /tmp/code_latest_amd64.deb
 sudo killall code
 code
